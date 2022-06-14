5035c4546d880325674b4b451080625736b4afc2
YAN LAZAROUSKI <57039899+TheRijex@users.noreply.github.com>
2022-06-14 21:26:04 +0300

    Add files via upload

--- gg/src/js/script.js
+++ gg/src/js/script.js
@@ -2,48 +2,79 @@
 document.addEventListener('DOMContentLoaded', () => {
     const tagDB = {
         tags: [],
-        privat: false,
+        add: true,
 
         get fullPulOfTags() {
-            return `${this.tags}`;
+            return this.tags;
           },
         set fullPulOfTags(value) {
             this.tags.push(value);
         },
         get privatStat() {
-            return this.privat;
+            return this.add;
         },
         set privatStat(value) {
-            this.privat = value;
+            this.add = value;
+        },
+
+        clearTags() {
+            this.tags = [];
+        },
+
+        togleAddstatus(){
+            this.add = !this.add;
         }
     };
-
-    const  addForm = document.querySelector('.bub'),
+    
+  
+    const  addForm = document.querySelector('#bub1'),
+           delForm = document.querySelector('#bub2'),
+           locate = document.querySelector('#bub3'),
            addInput = document.querySelector('.main__inp'),
            tagList = document.querySelector('.tag__box'),
            check  = document.querySelector('.checker');
     
+    if (localStorage.getItem('isChecked') === 'false') {
+        check.checked = true;
+        tagDB.privatStat = false;
+    }
+        
+    if (localStorage.getItem('localList')) {
+        console.log(localStorage.getItem('localList'));
+
+        localStorage.getItem('localList').split(',').forEach((item) => {
+            tagDB.fullPulOfTags = item;
+        });
+    }
+
     addForm.addEventListener('click', (event) => {
         event.preventDefault();
         let newTag = addInput.value;
-        if (newTag && !tagDB.privat) {
+        if (newTag && tagDB.privatStat) {
             if (newTag.length > 10 ) {
                 newTag = `${newTag.substring(0, 10)}...`;
             }
             tagDB.fullPulOfTags = newTag;
             createTagList(tagDB.tags, tagList);
         }
-        addInput.value = '';
-        
+        addInput.value = ''; 
+    });
+
+    delForm.addEventListener('click', (event) => {
+        event.preventDefault();
+        tagDB.clearTags();
+        createTagList(tagDB.tags, tagList);
+        localStorage.removeItem('localList')
     });
 
     check.addEventListener('change', () => {
         if (tagDB.privatStat) {
-            tagDB.privatStat = false;
+            tagDB.togleAddstatus();
+            localStorage.setItem('isChecked', false);
          } else {
-            tagDB.privatStat = true;
+            tagDB.togleAddstatus();
+            localStorage.setItem('isChecked', true);
         }
-        
     });
 
     function createTagList(tags, parent) {
@@ -67,6 +98,9 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
+    locate.addEventListener('click', () => {
+        localStorage.setItem('localList', tagDB.fullPulOfTags);
+    })
 
     createTagList(tagDB.tags, tagList);
 });
